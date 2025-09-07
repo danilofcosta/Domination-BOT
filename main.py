972bdeb7d85053cbc0b3e2d439436aeb680dfde8
Danilo <117859128+danilofcosta@users.noreply.github.com>
2025-09-07 17:43:26 -0300

    feat: Refactor bot initialization to use DominationDB and remove unused WAIFU bot code

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from domination import Domination
 from settings import Settings
 from pyrogram import idle
 from types_ import TipoCategoria
-
+from domination_db import DominationDB
 
 def main():
     var = Settings()
-    bot = Domination(
-        name="WAIFU",
-        api_id=var.API_ID,
-        api_hash=var.API_HASH,
-        bot_token=var.WAIFU_TK,
-        genero=TipoCategoria.WAIFU,
-        group_main=var.GROUP_MAIN,
-    )
-    bot2 = Domination(
+    # bot = Domination(
+    #     name="WAIFU",
+    #     api_id=var.API_ID,
+    #     api_hash=var.API_HASH,
+    #     bot_token=var.WAIFU_TK,
+    #     genero=TipoCategoria.WAIFU,
+    #     group_main=var.GROUP_MAIN,
+    # )
+    bot = DominationDB(
         name="HUS",
         api_id=var.API_ID,
         api_hash=var.API_HASH,
@@ -25,11 +25,9 @@ def main():
     )
 
     bot.start()
-    bot2.start()
 
     bot.send_message(chat_id="dog244", text="Bot iniciado")
-    bot2.send_message(chat_id="dog244", text="Bot iniciado")
-    # bot.me= bot.get_me()ienine']
+
     print("rodando bot")
 
     idle()
