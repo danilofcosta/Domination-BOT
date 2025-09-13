f10a322ef953c6e541f8d235b9717b081b464976
Danilo Costa <117859128+danilofcosta@users.noreply.github.com>
2025-09-13 16:16:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import asyncio,logging
+import logging
 from domination import Domination
 from domination_db import DominationDB
 from settings import Settings
 from pyrogram import idle
 from types_ import TipoCategoria
 
-logging.basicConfig(level=logging.CRITICAL,
-                    format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.CRITICAL,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 def main():
     var = Settings()
+
+    # Inicializa bots
     _waifu = Domination(
         name="WA",
         api_id=var.API_ID,
@@ -17,6 +22,8 @@ def main():
         genero=TipoCategoria.WAIFU,
         group_main=var.GROUP_MAIN,
     ).start()
+    _waifu.send_message("dog244", "WAIFU bot iniciado 🚀")
+
     _husbando = Domination(
         name="HUS",
         api_id=var.API_ID,
@@ -25,6 +32,8 @@ def main():
         genero=TipoCategoria.HUSBANDO,
         group_main=var.GROUP_MAIN,
     ).start()
+    _husbando.send_message("dog244", "HUSBANDO bot iniciado 🚀")
+
     bot_db = DominationDB(
         name="HUS_db",
         api_id=var.API_ID,
@@ -33,6 +42,8 @@ def main():
         genero=TipoCategoria.HUSBANDO,
         group_main=var.GROUP_MAIN,
     ).start()
+    bot_db.send_message("dog244", "HUSBANDO DB iniciado 📚")
+
     bot_db2 = DominationDB(
         name="waifudb",
         api_id=var.API_ID,
@@ -41,16 +52,10 @@ def main():
         genero=TipoCategoria.WAIFU,
         group_main=var.GROUP_MAIN,
     ).start()
+    bot_db2.send_message("dog244", "WAIFU DB iniciado 📚")
 
-
-
-
-    print("rodando bot")
-
-    idle()
-
+    print("rodando bots")
+    idle()  # Mantém os bots ativos
 
 if __name__ == "__main__":
     main()
-    
-    
