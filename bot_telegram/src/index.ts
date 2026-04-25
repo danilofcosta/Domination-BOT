0fc0798679fd2c4e60b592398edb5864d9fb737c
Danilo <117859128+danilofcosta@users.noreply.github.com>
2026-04-25 17:45:16 -0300

    .

--- bot_telegram/src/index.ts
+++ bot_telegram/src/index.ts
@@ -6,7 +6,7 @@ import { testDBConnection } from "./utils/testes/test_db_Connection.js";
 import { ChatType, NODE_ENV } from "./utils/customTypes.js";
 import { fatal, info } from "./utils/log.js";
 import { RunPolling } from "./index_Polling.js";
-// import { RunWebHook } from "./index_webhook.js";
+import { RunWebHook } from "./index_webhook.js";
 
 await Environment_validation();
 await testDBConnection();
@@ -23,17 +23,21 @@ if (!BOT_TOKEN) {
 
 const bot = await initializeBot(process.env.TYPE_BOT as ChatType, BOT_TOKEN);
 
-// await bot.api.deleteWebhook({ drop_pending_updates: true });
-// info("Bot instanciado com sucesso");
+await bot.api.deleteWebhook({ drop_pending_updates: true });
+info("Bot instanciado com sucesso");
 
-if (process.env.NODE_ENV === NODE_ENV.DEVELOPMENT ||  !process.env.ENDPOINT) {
+if (process.env.NODE_ENV === NODE_ENV.DEVELOPMENT || !process.env.ENDPOINT) {
   await RunPolling(bot, true);
-} else if (process.env.NODE_ENV === NODE_ENV.PRODUCTION) {
-  // await RunWebHook(
-  //   process.env.TYPE_BOT?.toLowerCase() ?? "waifu",
-  //   bot,
-  //   process.env.ENDPOINT!,
-  // );
+} else if (
+  process.env.NODE_ENV === NODE_ENV.PRODUCTION &&
+  process.env.ENDPOINT
+) {
+  await RunWebHook(
+    process.env.TYPE_BOT?.toLowerCase() ?? "waifu",
+    bot,
+    process.env.ENDPOINT!,
+  );
 } else {
   info("NODE_ENV não definido corretamente");
+   await RunPolling(bot, true);
 }
