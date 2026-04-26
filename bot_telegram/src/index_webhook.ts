e4081888d34032ae726dc3670e93019836a20c07
Danilo <117859128+danilofcosta@users.noreply.github.com>
2026-04-26 08:47:36 -0300

    modified:   bot_telegram/src/index_webhook.ts

--- bot_telegram/src/index_webhook.ts
+++ bot_telegram/src/index_webhook.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { webhookCallback } from "grammy";
 //import { info } from "./utils/log.js";
-
+let init  :boolean = false
 const app = express();
 
 app.use(express.json());
@@ -14,11 +14,12 @@ app.use(express.json());
 
 app.use(webhookCallback(bot, "express"));
 
- if (process.env.CHAT_ID_DEV) {
+ if (process.env.CHAT_ID_DEV && !init) {
         await bot.api.sendMessage(
           process.env.CHAT_ID_DEV as string,
           `Bot Iniciado ${process.env.TYPE_BOT}\nModo : ${process.env.NODE_ENV}`,
         );
+        init =true
       }
 
       return app;
