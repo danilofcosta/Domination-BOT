041e511741eaab90bb48bc0bd565041034138db5
Danilo <117859128+danilofcosta@users.noreply.github.com>
2026-04-25 22:58:39 -0300

    .

--- bot_telegram/src/index_webhook.ts
+++ bot_telegram/src/index_webhook.ts
@@ -28,7 +28,12 @@ export async function RunWebHook(
   app.use(express.json());
 
   app.use(webhookCallback(bot, "express"));
+
   // app.listen(port, () => {
   //   console.log(`[WEBHOOK] ${TYPE_BOT} rodando em ${path} na porta ${port}`);
   // });
+   await bot.api.sendMessage(
+          process.env.CHAT_ID_DEV as string,
+          `Bot Iniciado ${process.env.TYPE_BOT}\nModo : ${process.env.NODE_ENV}\nConectado ao banco: `,
+        );
 }
