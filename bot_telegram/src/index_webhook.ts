5193ca24ee269beb264d55cbc27b5301ca01373c
Danilo <117859128+danilofcosta@users.noreply.github.com>
2026-04-27 22:02:36 -0300

    modified:   bot_telegram/src/index_webhook.ts

--- bot_telegram/src/index_webhook.ts
+++ bot_telegram/src/index_webhook.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { webhookCallback } from "grammy";
 
-let init = false;
+//let init = false;
 
 export async function RunWebHook(bot) {
   console.log("RODANDO BOT Webhook");
@@ -12,13 +12,13 @@ export async function RunWebHook(bot) {
   // 🔥 rota específica (ESSENCIAL)
   app.post("/webhook", webhookCallback(bot, "express"));
 
-  if (process.env.CHAT_ID_DEV && !init) {
-    await bot.api.sendMessage(
-      process.env.CHAT_ID_DEV,
-      `Bot Iniciado ${process.env.TYPE_BOT}\nModo : ${process.env.NODE_ENV}`
-    );
-    init = true;
-  }
+  // if (process.env.CHAT_ID_DEV && !init) {
+  //   await bot.api.sendMessage(
+  //     process.env.CHAT_ID_DEV,
+  //     `Bot Iniciado ${process.env.TYPE_BOT}\nModo : ${process.env.NODE_ENV}`
+  //   );
+  //   init = true;
+  // }
 
   return app;
 }
